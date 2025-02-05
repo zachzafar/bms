c504e2debf70dbbf4e693ae201aa63943e9f5040
Marcus Benskin <zacharyakeil1998@outlook.com>
2025-02-05 09:24:36 -0400

    asset-type service fix

--- apps/api/src/schema-design/asset-type/asset-type.service.ts
+++ apps/api/src/schema-design/asset-type/asset-type.service.ts
@@ -3,6 +3,7 @@ import { MySql2Database } from 'drizzle-orm/mysql2';
 import { DrizzleAsyncProvider } from 'src/drizzle/drizzle.provider';
 import * as schema from '@repo/api-contract';
 import type { InsertAssetType, UpdateAssetType } from '@repo/api-contract';
+import type { InsertAssetProperty, UpdateAssetProperty } from '@repo/api-contract';
 import { eq } from 'drizzle-orm';
 
 @Injectable()
@@ -34,22 +35,22 @@ export class AssetTypeService {
         return this.db.delete(schema.AssetType).where(eq(schema.AssetType.id, id));
     }
 
-    async createProperty(data: InsertProperty) {
-        const newPropertyId = await this.db.insert(schema.Property).values(data).$returningId().execute();
+    async createProperty(data: InsertAssetProperty) {
+        const newPropertyId = await this.db.insert(schema.assetProperty).values(data).$returningId().execute();
         
         return this.getProperty(newPropertyId[0].id);
     }
 
     async getProperty(id: number) {
-        return this.db.query.Property.findFirst({ where: (property, { eq }) => eq(property.id, id) });
+        return this.db.query.assetProperty.findFirst({ where: (property, { eq }) => eq(property.id, id) });
     }
 
     async getProperties() {
-        return this.db.query.Property.findMany();
+        return this.db.query.assetProperty.findMany();
     }
 
-    async updateProperty(id: number, data: UpdateProperty) {
-        await this.db.update(schema.Property).set(data).where(eq(schema.Property.id, id)).execute()
+    async updateProperty(id: number, data: UpdateAssetProperty) {
+        await this.db.update(schema.assetProperty).set(data).where(eq(schema.assetProperty.id, id)).execute()
         return this.getProperty(id);
     }
 
