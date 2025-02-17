f677adc0bc0c96093463506a2b9b3ce8623a9ad6
Zach Harris <zachary.harris314@protonmail.com>
2025-02-17 12:07:10 -0400

    added domain to cors policy

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000','http://147.182.188.77'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
