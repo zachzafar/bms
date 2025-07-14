019fc710220f9c3a5a26dd12a9e52d2d0883e4e8
Zach Harris <zachary.harris314@protonmail.com>
2025-07-14 17:34:10 -0400

    added env variables to correct location

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,6 +17,10 @@ module.exports = {
           REFRESH_JWT_SECRET:"3fb3b3c67fb2b966540d4f0033cdc6035dda30cd41083f176625008d7f2a2768382bc888aeb1a88ed95f2e90b0c9e0c8cdf2b7b872db95376d7c0ee472a25f61",
           REFRESH_JWT_EXPIRES_IN:"1d",
           JWT_EXPIRES_IN:"5m",
+          SPACES_ENDPOINT:"https://nyc3.digitaloceanspaces.com",
+          SPACES_KEY:"DO801XR9LC", 
+          SPACES_SECRET:"RGgk9lSeGiCkG4jrFEiB5qW8sgmbz3YxDbYsc58gXhM",
+          SPACES_REGION:"nyc3",
           FRONTEND_URL:"https://bookos.xyz",
           RESEND: "re_WDpqRKN1_Lq3RrVzL6Mp72S6HoDKeFSdH",
           JWT_SECRET:"ce726e971a40f3f5b419ae61ab6ff9285564f465608b2a01d113c6b01eaf87749376ca7c887f2e8638e8b1996a6c7d0472e982d1412375fedbd7d14a684c56d6"
